1c42566ac2b47c86f4b9d66172f8d21792dc9b1d
1107tomkim <3268@holbertonschool.com>
2021-08-05 01:11:18 +0000

    updated header file

--- holberton.h
+++ holberton.h
@@ -1,4 +1,30 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+
+/**
+ * struct search -  
+ *
+ *
+ */
+typedef struct search
+{
+	char character;
+	int (*function)(int count, va_list list);
+} st_search;
+
+int _putchar(char c);
+int print_char(int count, va_list c);
+int print_string(int count, va_list s);
+int print_int(int count, va_list i);
+int print_uint(int count, va_list u);
+int print_percent(int count, va_list p);
+int get_op_func(int count, const char *format, unsigned int *ptr, va_list list);
+
+int _printf(const char *format, ...);
 #endif
